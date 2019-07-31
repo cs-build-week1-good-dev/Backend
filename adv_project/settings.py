da6c98da45bc0aef7198291d28b8546e0a407c75
chasegarsee <chasegarseesoftwaredevelopment@gmail.com>
2019-07-31 18:19:19 -0500

    trying another thing

--- adv_project/settings.py
+++ adv_project/settings.py
@@ -105,7 +105,7 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.TokenAuthentication',
     ),
 }
-CORS_ORIGIN_ALLOW_ALL=True
+CORS_ORIGIN_ALLOW_ALL = False
 
 CORS_ALLOW_HEADERS = list(default_headers) + [
     'accept',
